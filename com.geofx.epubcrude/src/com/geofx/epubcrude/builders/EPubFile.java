dd4571ae459720763bb0a109887065aa6743c1b9
Ric Wright <rkwright@geofx.com>
2014-09-26 16:21:59 -0500

    More copyright updates

--- com.geofx.epubcrude/src/com/geofx/epubcrude/builders/EPubFile.java
+++ com.geofx.epubcrude/src/com/geofx/epubcrude/builders/EPubFile.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006-2013 Ric Wright - Geo-F/X
+ * Copyright (c) 2006-2014 Ric Wright - Geo-F/X
  * 
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Eclipse Public License v1.0
--- com.geofx.epubcrude/src/com/geofx/epubcrude/builders/ResourceChangeReporter.java
+++ com.geofx.epubcrude/src/com/geofx/epubcrude/builders/ResourceChangeReporter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013 Ric Wright - Geo-F/X
+ * Copyright (c) 2013-14 Ric Wright - Geo-F/X
  * 
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Eclipse Public License v1.0
